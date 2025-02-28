7c53419ecf3f4156c0cc118aae5f14b6244cc17a
Shubhangam Garg <shubhangam_g@iirs.gov.in>
2025-02-28 14:37:41 +0530

    CORS changes

--- src/main.py
+++ src/main.py
@@ -2,9 +2,11 @@ from flask import Flask
 from server.routes import main  # Import routes
 from builder import metadata_builder
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(main)
+CORS(app) 
 
 if __name__ == "__main__":
     metadata_builder.build()
