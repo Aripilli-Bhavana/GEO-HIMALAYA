716e03e52eb33191b9949c86c182baa4f543698c
Shubhangam Garg <shubhangam_g@iirs.gov.in>
2025-02-28 15:28:39 +0530

    Geojson processing changes

--- src/helper/response_helper.py
+++ src/helper/response_helper.py
@@ -4,7 +4,7 @@ from flask import jsonify
 def get_result_from_db(llm_response : str, aoi : str) :
     result = database_helper.run_query(llm_response, aoi)
     if result[0] : 
-        return True, response[1]
+        return True, result[1]
     else :
         error_str = f"{result[1]}"
         response = jsonify({
