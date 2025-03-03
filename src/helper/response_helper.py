8d7b452fb3a78423f6c4bf7057614224bf2f9bfa
Shubhangam Garg <shubhangam_g@iirs.gov.in>
2025-03-03 09:43:21 +0530

    Response changes

--- src/helper/response_helper.py
+++ src/helper/response_helper.py
@@ -4,10 +4,15 @@ from flask import jsonify
 def get_result_from_db(llm_response : str, aoi : str) :
     result = database_helper.run_query(llm_response, aoi)
     if result[0] : 
-        return True, result[1]
+        response = jsonify({
+            "sql_query" : llm_response,
+            "data" : result[1]
+        })
+        return True, response
     else :
         error_str = f"{result[1]}"
         response = jsonify({
+            "sql_query" : llm_response,
            "error" : error_str
         })
         return False, response
